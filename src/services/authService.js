7e86baa077b7c1eaf5680253a43ba5493d31cc47
jiiminhong <chunsim015@gmail.com>
2024-07-02 10:54:33 +0900

    feat: 네이버 소셜로그인 docker, db 연결

--- src/services/authService.js
+++ src/services/authService.js
@@ -26,21 +26,38 @@ const naverCallback = async (req, res) => {
 
     console.log('유저 데이터 db로 insert:', user);
 
-    con.query(
-      'INSERT INTO user (u_id, u_name, u_nickname, u_email, u_birth, u_img) VALUES (?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE u_name = VALUES(u_name), u_nickname = VALUES(u_nickname), u_email = VALUES(u_email), u_birth = VALUES(u_birth), u_img = VALUES(u_img)',
-      [id, name, nickname, email, `${birthyear}-${birthday}`, profile_image],
-      (error, results) => {
-        if (error) {
-          console.error('DB Error:', error);
-          return res.status(500).send('회원 정보를 가져오지 못했습니다.');
-        }
-
-        const token = generateToken(user);
-        // 리다이렉션
-        const redirectUrl = `http://localhost:3000/auth/success?token=${token}`;
-        res.redirect(redirectUrl);
+    // 이미 존재하는 회원인지 검사
+    con.query('SELECT * FROM user WHERE u_id = ?', [id], (err, results) => {
+      if (err) {
+        console.error('DB Error:', err);
+        return res.status(500).send('회원 정보를 가져오지 못했습니다.');
       }
-    );
+
+      // 회원 정보가 이미 존재하는 경우 삽입하지 않고 로그인 진행
+      if (results.length > 0) {
+        console.log('회원 정보가 이미 존재합니다.');
+        const token = generateToken(user);
+        const redirectUrl = `http://localhost:3000/auth/success?token=${token}`;
+        return res.redirect(redirectUrl);
+      } else {
+        // 회원 정보가 존재하지 않는 경우 삽입
+        con.query(
+          'INSERT INTO user (u_id, u_name, u_nickname, u_email, u_birth, u_img) VALUES (?, ?, ?, ?, ?, ?)',
+          [id, name, nickname, email, `${birthyear}-${birthday}`, profile_image],
+          (error, results) => {
+            if (error) {
+              console.error('DB Insert Error:', error);
+              return res.status(500).send('회원 정보를 가져오지 못했습니다.');
+            }
+
+            console.log('User inserted successfully');
+            const token = generateToken(user);
+            const redirectUrl = `http://localhost:3000/auth/success?token=${token}`;
+            res.redirect(redirectUrl);
+          }
+        );
+      }
+    });
   } catch (error) {
     res.status(500).send('네이버 로그인에 실패했습니다.');
   }
