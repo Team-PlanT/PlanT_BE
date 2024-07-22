1c3312e8b6ff3447c84146a8a86db6433f533647
Taehoon Kim <thk8lue@gmail.com>
2024-07-22 10:41:34 +0900

    Feat: searchController 완성

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -13,32 +13,46 @@ const queryDatabase = (query, params) => {
 
 const searchPost = async (req, res) => {
   const query = req._parsedUrl.query.substring(6);
-  const likeQuery = `%${query}%`; // 부분 일치를 위한 쿼리
-
+  const decodedQuery = decodeURIComponent(query);
+  const likeQuery = `%${decodedQuery}%`; // 부분 일치를 위한 쿼리
   try {
-    const results0 = await queryDatabase(
+    const records0 = await queryDatabase(
       "SELECT * FROM post WHERE p_title LIKE ? ORDER BY p_id DESC",
       [likeQuery]
     );
-    const results1 = await queryDatabase(
+    const records1 = await queryDatabase(
       "SELECT * FROM plan WHERE pl_schedule LIKE ? ORDER BY p_id DESC",
       [likeQuery]
     );
-    const results2 = await queryDatabase(
+    const records2 = await queryDatabase(
       "SELECT * FROM plan WHERE pl_place LIKE ? ORDER BY p_id DESC",
       [likeQuery]
     );
 
     // 각 쿼리의 결과를 결합
-    let combinedResults = [...results0, ...results1, ...results2];
+    let combinedRecords = [...records0, ...records1, ...records2];
 
     // p_id 기준으로 오름차순 정렬하면서 중복 제거
-    combinedResults = combinedResults
+    combinedRecords = combinedRecords
       .sort((a, b) => b.p_id - a.p_id)
       .filter(
         (item, index, self) =>
           index === self.findIndex((t) => t.p_id === item.p_id)
       );
+
+    const postIds = combinedRecords.map((record) => record.p_id);
+
+    const combinedResults = [];
+
+    for (const postId of postIds) {
+      const result = await queryDatabase(
+        "SELECT * FROM post WHERE p_id = ? ORDER BY p_id DESC",
+        [postId]
+      );
+      combinedResults.push(...result);
+    }
+
+    console.log("combinedResults:", combinedResults);
 
     res.status(200).json({ posts: combinedResults });
   } catch (error) {
