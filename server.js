bcb88d184ccd6da4aa581cab88abf889a4fd6455
elk <flution221@gmail.com>
2024-07-23 11:57:24 +0900

    "Fix : server.js / detail 부분 수정"

--- server.js
+++ server.js
@@ -12,21 +12,19 @@ const postRoutes = require("./src/routes/postRoutes");
 const planRoutes = require("./src/routes/planRoutes");
 const listRoutes = require("./src/routes/listRoutes");
 const searchRoutes = require("./src/routes/searchRoutes");
+const detailRoutes = require("./src/routes/detailRoutes");
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-
 app.use(
   cors({
-    // 프론트엔드 주소
     origin: "http://localhost:3000",
     credentials: true,
   })
 );
-
 
 app.use(express.json());
 
@@ -38,7 +36,6 @@ const sessionStore = new MySQLStore({
   database: process.env.DB_NAME,
 });
 
-// 세션
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -59,13 +56,12 @@ app.use("/api/posts", postRoutes);
 app.use("/api/plans", planRoutes);
 app.use("/api/list", listRoutes);
 app.use("/api/search", searchRoutes);
+app.use("/api/detail", detailRoutes);
 
-// DB 연결 끊겼을 때 재연결 코드
 con.on("error", (err) => {
   console.log("db error", err);
   if (err.code === "PROTOCOL_CONNECTION_LOST") {
     console.log("Reconnecting to the database...");
-
     con.connect((err) => {
       if (err) {
         console.error("Error reconnecting to the database:", err);
