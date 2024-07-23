63b6106aa10da23a7bb7f7b7d85f8d7b355b0373
jiiminhong <chunsim015@gmail.com>
2024-07-23 11:20:04 +0900

    feat: post route rollback

--- src/routes/planRoutes.js
+++ src/routes/planRoutes.js
@@ -1,20 +1,8 @@
 const express = require('express');
+const { createPlan } = require('../controllers/planController');
+
 const router = express.Router();
-const con = require('../config/database');
 
-router.get('/', (req, res) => {
-  const { p_id } = req.query;
-  console.log(`Fetching plans for p_id: ${p_id}`); // 쿼리 로그 추가
-  const query = 'SELECT * FROM plan WHERE p_id = ? ORDER BY pl_startTime';
-
-  con.query(query, [p_id], (err, results) => {
-    if (err) {
-      console.error('Error fetching plans:', err);
-      return res.status(500).json({ error: 'Error fetching plans' });
-    }
-    console.log('Fetched plans:', results); // 결과 로그 추가
-    res.json(results);
-  });
-});
+router.post('/', createPlan);
 
 module.exports = router;
