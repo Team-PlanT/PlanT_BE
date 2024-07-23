1acf937cc33b49f365e9df0aff16cd0bd3cfe0a4
elk <flution221@gmail.com>
2024-07-23 11:27:03 +0900

    "Fix : 파일 없는거 수정"

--- src/controllers/detailController.js
+++ src/controllers/detailController.js
@@ -1,5 +1,6 @@
 const con = require('../config/database');
 
+
 const getPlansById = (req, res) => {
   const { id } = req.params;
   con.query('SELECT * FROM plan WHERE p_id = ? ORDER BY pl_date ASC, pl_startTime ASC', [id], (err, results) => {
